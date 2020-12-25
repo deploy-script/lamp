d8b63da2ed14bbaccfce811efd8bc72e9972b698
Lawrence Cherone <lawrence@cherone.co.uk>
2020-12-25 19:11:28 +0000

    .

--- script.sh
+++ script.sh
@@ -193,7 +193,7 @@ change_php_ini() {
 
     sed -i 's/max_file_uploads\s*=.*/max_file_uploads = 5/g' /etc/php/$PHP_VERSION/apache2/php.ini
 
-    sed -i 's/upload_tmp_dir\s*=.*/upload_tmp_dir = \/tmp\/php-uploads/g' /etc/php/$PHP_VERSION/apache2/php.ini
+    sed -i "s/.*upload_tmp_dir.*/upload_tmp_dir = \/tmp\/php-uploads/" /etc/php/$PHP_VERSION/apache2/php.ini
 }
 
 #
